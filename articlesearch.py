842cd3a2eff29da5ac9c91c14a5ce247c97b9018
Laya Dang <pd03@bu.edu>
2023-05-25 17:40:21 -0400

    scholarsearch() returns link to first result

--- articlesearch.py
+++ articlesearch.py
@@ -6,10 +6,17 @@ excelfile = pd.read_excel("articlestest.xlsx")
 excelfile.to_csv("articlestest.csv", index=None,header=True)
 df = pd.DataFrame(pd.read_csv("articlestest.csv"))
 
-url = "https://scholar.google.com"
+url = "https://scholar.google.com/scholar?q="
 def scholarsearch():
     article=df.Name[0].replace(" ", "-")
     req = requests.get(url+ "/scholar?q=" + article)
     soup = BeautifulSoup(req.text, 'html.parser')
+    headerres = soup.find('h3', {'class': 'gs_rt'})
+
+    if headerres:
+        linkres = headerres.find('a')['href']
+        return linkres
+    
+    return None
 
 scholarsearch()
